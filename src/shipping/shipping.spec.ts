87332de153c699b95069392aa311a7abe40c762c
mrbrunelli <matheus.brunelli@gmail.com>
2022-03-26 18:42:57 -0300

    test: ensure shipping calc return free shipping cost if calculated value is less than 10

--- src/shipping/shipping.spec.ts
+++ src/shipping/shipping.spec.ts
@@ -1,11 +1,27 @@
 import { CalculateShipping } from "./calculate-shipping";
-import { ShippingService } from "./shipping";
+import { Shipping } from "./shipping";
+
+const makeSut = () => {
+  const calculateShipping = new CalculateShipping();
+  const sut = new Shipping(calculateShipping);
+
+  return {
+    calculateShipping,
+    sut,
+  };
+};
 
 describe("ShippingService", () => {
   test("should be defined", () => {
-    const calculateShipping = new CalculateShipping();
-    const shippingService = new ShippingService(calculateShipping);
+    const { sut } = makeSut();
 
-    expect(shippingService).toBeDefined();
+    expect(sut).toBeDefined();
+  });
+
+  test("should return free shipping cost if calculated value is less than R$ 10,00", () => {
+    const { sut } = makeSut();
+
+    const result = sut.getCost({ distance: 44, weight: 7 });
+    expect(result).toBe(0);
   });
 });
