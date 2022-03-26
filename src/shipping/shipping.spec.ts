bb675e38ed549fe8a5c141a8b50b64b0d64a9561
mrbrunelli <matheus.brunelli@gmail.com>
2022-03-26 19:26:07 -0300

    refactor: simplify sut creation

--- src/shipping/shipping.spec.ts
+++ src/shipping/shipping.spec.ts
@@ -1,37 +1,30 @@
 import { CalculateShipping } from "./calculate-shipping";
 import { Shipping } from "./shipping";
 
-const makeSut = () => {
-  const calculateShipping = new CalculateShipping();
-  const sut = new Shipping(calculateShipping);
-
-  return {
-    calculateShipping,
-    sut,
-  };
-};
-
 describe("ShippingService", () => {
-  test("should be defined", () => {
-    const { sut } = makeSut();
+  let calculateShipping: CalculateShipping;
+  let sut: Shipping;
 
+  beforeEach(() => {
+    calculateShipping = new CalculateShipping();
+    sut = new Shipping(calculateShipping);
+  });
+
+  test("should be defined", () => {
     expect(sut).toBeDefined();
   });
 
   test("should return free shipping cost if calculated value is less than R$ 10,00", () => {
-    const { sut } = makeSut();
     const result = sut.getCost({ distance: 44, weight: 7, rate: 0.02 });
     expect(result).toBe(0);
   });
 
   test("should return the shipping cost amount to be paid by the customer", () => {
-    const { sut } = makeSut();
     const result = sut.getCost({ distance: 100, weight: 9, rate: 0.02 });
     expect(result).toBe(18);
   });
 
   test("should calls with correctly params", () => {
-    const { sut } = makeSut();
     const spy = jest.spyOn(sut, "getCost");
 
     sut.getCost({ distance: 100, weight: 9, rate: 0.02 });
@@ -42,7 +35,6 @@ describe("ShippingService", () => {
   });
 
   test("should calls calculate shipping with correclty params", () => {
-    const { sut, calculateShipping } = makeSut();
     const spy = jest.spyOn(calculateShipping, "calculate");
 
     sut.getCost({ distance: 100, weight: 9, rate: 0.02 });
